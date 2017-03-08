8e384813742d77c141784586a23b710902aefec9
Macky Beltran <mackybeltran@Mackys-MacBook-Pro.local>
2017-03-08 18:42:40 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx'
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+    <div>
+      <nav className="navbar">
+        <h1>Chatty</h1>
+      </nav>
+
+      <MessageList />
+      <ChatBar />
+    </div>
     );
   }
 }
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
