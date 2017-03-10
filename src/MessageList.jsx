13af95a2a8ec6a7d5965fe11e459556f420fda2a
Macky Beltran <mackybeltran@Mackys-MacBook-Pro.local>
2017-03-10 17:43:28 -0500

    adding react component new message

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import Message from './Message.jsx'
 
 
 class MessageList extends React.Component {
   render() {
   return (
-         <main className="messages">
-            <div className="message">
-              <span className="message-username">Anonymous1</span>
-              <span className="message-content">I won't be impressed with technology until I can download food.</span>
-            </div>
-            <div className="message system">
-              Anonymous1 changed their name to nomnom.
-            </div>
-        </main>
+    <div id="message-list">
+      {this.props.messages.map((message, index) => {
+        return (
+          <Message key={index} username={message.username} content={message.content} />)
+
+      }
+      )} </div>
+
       )
     }
 }
